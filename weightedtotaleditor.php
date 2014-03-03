952b32107508abbab93db2d0d8cf120e8c57affc
ARyaskov <ARyaskov@localhost>
2014-03-03 17:58:21 +0400

    Added weightedtotal editor class.

---
+++ weightedtotaleditor.php
@@ -0,0 +1,42 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Grading method controller for the Weightedtotal plugin
+ *
+ * @package    gradingform
+ * @subpackage weightedtotal
+ * @author     Andrew Ryaskov
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once("HTML/QuickForm/input.php");
+
+class MoodleQuickForm_weightedtotaleditor extends HTML_QuickForm_input {
+
+    function MoodleQuickForm_weightedtotaleditor($elementName=null, $elementLabel=null, $attributes=null) {
+        parent::HTML_QuickForm_input($elementName, $elementLabel, $attributes);
+    }
+
+    public function toHtml() {
+    }
+
+    public function exportValue(&$submitValues, $assoc = false) {
+    }
+}
